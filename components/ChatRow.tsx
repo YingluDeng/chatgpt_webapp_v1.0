3a54c608cbe3ed9c29f848f60a24fcb20f0f9dd6
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-04-21 22:45:26 -0700

    active chat row shown

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,28 +1,45 @@
 import { db } from "@/firebase";
-import { deleteDoc, doc } from "@firebase/firestore";
+import { collection, deleteDoc, doc } from "@firebase/firestore";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline"
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 type Props = {
     id: string;
 }
 
 function ChatRow({id} : Props) {
+    const pathname = usePathname();
+    const router = useRouter();
+    const { data: session } = useSession();
+    const [ active, setActive ] = useState(false);
+
+    const [messages] = useCollection(
+        collection(db, "users", session?.user?.email!, "chats", id, "messages")
+    );
+
     // const removeChat = async() => {
     //     await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
     //     router.replace("/");
     // }
+    useEffect(() => {
+        if (!pathname) return;
+
+        setActive(pathname.includes(id));
+    }, [pathname]);
+    
 
     return (
         <Link
             href={`/chat/${id}`}
-            className={`chatRow justify-center `}
-            // ${active && "bg-gray-700/50"}
+            className={`chatRow justify-center ${active && "bg-gray-700/50"}`}
         >
             <ChatBubbleLeftIcon className="h-5 w-5" />
-            <p className="flex-1 hidden md:inline-flex truncare">New Chat</p>
-            {/* {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"} */}
+            <p className="flex-1 hidden md:inline-flex truncare">{messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}</p>
             <TrashIcon  className="h-5 w-5 text-white hover:text-red-700" />   
             {/* onClick={removeChat} */}
         </Link>
