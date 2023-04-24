bac48d1849ce4b7825afb7dd5bda46ca0d944f28
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-04-24 02:32:06 -0700

    message page done

--- components/Message.tsx
+++ components/Message.tsx
@@ -5,9 +5,14 @@ type Props = {
 };
 
 function Message({message} : Props) {
+    const isChatGPT = message.user.name == "ChatGPT";
+
     return (
-        <div>
-            Message
+        <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
+            <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+                <img src={message.user.avatar} alt="" className="h-8 w-8 rounded-sm" />
+                <p className="pt-1 text-sm">{message.text}</p>
+            </div>
         </div>
     )
 };
