4b5cd1b5b339b2ac710e82c53fa3204faf8f60f1
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-04-24 19:34:27 -0700

    swr trial

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,9 +1,32 @@
+'use client'
+import useSWR from "swr";
+import Select from 'react-select';
+
+const fetchModels = () => fetch('/api/getEngines').then(res => res.json()) 
+
 function ModelSelection() {
+    const {data: models, isLoading} = useSWR('models', fetchModels);
+    const {data: model, mutate: setModel} = useSWR('model', {
+        fallbackData: 'text-davinci-003'
+    })
+
     return (
         <div>
-            ModelSelection
+            <Select 
+                className="mt-2"
+                options={models?.modelOptions}
+                defaultValue={model}
+                placeholder={model}
+                isSearchable
+                isLoading={isLoading}
+                menuPosition='fixed'
+                classNames={{
+                    control: (state) => "bg-[#434654] border-[#434543]"
+                }}
+                onChange={(e) => setModel(e.value)}
+            />
         </div>
     )
 }
 
-export default ModelSelection
+export default ModelSelection;
